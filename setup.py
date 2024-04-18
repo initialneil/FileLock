86cc91d108f9620af1a4134896aa34af250b135c
Neil Z. SHAO <initialneil@gmail.com>
2024-04-18 11:20:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     url='https://github.com/dmfrey/FileLock',
     license='LICENSE.txt',
     description='File locking library',
-    long_description=open('README.txt').read()
+    long_description=open('README.md').read()
 )
