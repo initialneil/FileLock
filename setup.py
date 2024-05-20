09c326a5ac1754d7efc456b3504e2172854bfb09
Neil Z. SHAO <initialneil@gmail.com>
2024-05-20 12:09:25 +0800

    rename to SimpleFileLock

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Evan Fosmark',
     author_email='me@evanfosmark.com',
-    packages=['simple_filelock','filelock.test'],
+    packages=['simple_filelock','simple_filelock.test'],
     url='https://github.com/dmfrey/FileLock',
     license='LICENSE.txt',
     description='File locking library',
